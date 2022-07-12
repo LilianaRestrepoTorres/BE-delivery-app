58f4d98c2e024910530ba4917ee3c41f9d13f75a
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-07-11 23:34:04 -0500

    Adding endpoints get robots by id and status

--- server.js
+++ server.js
@@ -5,15 +5,38 @@ const port = 9393;
 //Middleware
 app.use(express.json());
 
+const robots = [
+    {"id": 1, "name": "Robot 1", "status": "available"},
+    {"id": 2, "name": "Robot 2", "status": "busy"},
+    {"id": 3, "name": "Robot 3", "status": "reserved"},
+]
+
+// Main root of the server
 app.get('/', (req, res) => {
     res.status(200).send({message: 'Hello! This is backend of delivery app.'});
 })
 
-app.get('/:robot', (req, res) => {
-    const {robot} = req.params;
-    const {limit} = req.query;
-    console.log(robot, limit);
-    res.status(200).send({message: `You asked for a robot called ${robot} with limit ${limit}`});
+app.get('/robots', (req, res) => {
+    res.status(200).send(robots);
+})
+
+// Endpoint for getting robot by id
+app.get('/robots/:id', (req, res) => {
+    const {id} = req.params;
+    const robot = robots.find(robot => robot.id == id);
+    if (!robot) {
+        return res.status(404).send({message: 'Robot not found'});
+    }
+    res.status(200).send(robot);
+})
+
+// Endpoint for getting robot by status
+app.get('/robots/:status', (req, res) => {
+    const {status} = req.params;
+    if (!status) {
+        return res.status(404).send({message: 'Bad request'});
+    }
+    res.status(200).send({robots: robots.filter(robot => robot.status === status)});
 })
 
 app.post('/', (req, res) => {
